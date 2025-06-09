edffd567f005f31675f8e4a895596b4360e3c847
Anisa Cikal Virgifiani <160223842+Icaa03@users.noreply.github.com>
2025-06-10 00:40:23 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template
+import google.generativeai as genai
+from PIL import Image
+import markdown
+import os
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "upload"
+
+genai.configure(api_key="AIzaSyDcM_pNXTKB-zOCWPc2fYO8pYlG5b2_dCQ")  
+model = genai.GenerativeModel("gemini-2.0-flash")  
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    reply = ""
+    jenis_kulit = request.form["jenis_kulit"]
+    masalah_kulit = request.form["masalah_kulit"]
+    budget = request.form["budget"]
+    file = request.files["foto"]
+
+    if file.filename != "":
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+        os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
+        file.save(filepath)
+
+        img = Image.open(filepath)
+
+        prompt = f"""
+        Saya memiliki kulit {jenis_kulit} dan mengalami masalah {masalah_kulit}.
+        Budget saya untuk skincare adalah {budget}.
+        Berdasarkan foto wajah yang saya unggah, tolong rekomendasikan produk skincare yang cocok
+        beserta komposisi bahan yang direkomendasikan. Jika memungkinkan, sertakan nama dan gambar produk.
+        Jelaskan alasannya juga secara singkat.
+        """
+
+        response = model.generate_content([prompt, img])
+        reply_raw = response.text
+        reply = markdown.markdown(reply_raw)
+
+    return render_template("index.html", reply=reply)
+
+if __name__ == "__main__":
+    app.run(debug=True)
