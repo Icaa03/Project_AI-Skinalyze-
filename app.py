9f61a3b8774fefd2cd9641b21d3d705be6fd1e32
Anisa Cikal Virgifiani <160223842+Icaa03@users.noreply.github.com>
2025-06-19 10:57:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,5 @@ def analyze():
     return render_template("index.html", reply=reply)
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
